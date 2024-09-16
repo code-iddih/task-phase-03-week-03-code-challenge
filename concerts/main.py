66e47d07fe296e9298a2cc59b7b261c126bcd662
code-iddih <idinyamwaya@gmail.com>
2024-09-16 18:58:29 +0300

    Adding a sketch schema for concerts

--- concerts/main.py
+++ concerts/main.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import (
     create_engine,
     Column,
-
+    Integer,
+    String
 )
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -24,4 +25,8 @@ class venue:
     id : int pk
     title : str
     city : str
+
+#Association table (concerts)
+    band_id : int fk(bands.id)
+    venue_id : int fk(venues.id)
 '''
