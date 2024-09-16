24abb4fca52a5c7cdb3ba17d69a27e46d5a83436
code-iddih <idinyamwaya@gmail.com>
2024-09-16 21:08:12 +0300

    Adds the Band & Venue Modal

--- concerts/main.py
+++ concerts/main.py
@@ -30,3 +30,23 @@ class venue:
     band_id : int fk(bands.id)
     venue_id : int fk(venues.id)
 '''
+
+# Creating the modals
+
+class Band:
+    __tablename__ = 'bands'
+    id = Column(Integer() , primary_key = True)
+    name = Column(String() , nullable = False)
+    hometown = Column(String() , nullable = False)
+
+    def __rep__(self):
+        return f"<The Customer is : {self.name}>"
+    
+class Venue:
+    __tablename__ = 'venues'
+    id = Column(Integer() , primary_key = True)
+    title = Column(String() , nullable = False)
+    city = Column(String() , nullable = False)
+
+    def __rep__(self):
+        return f"<The Product is : {self.name}>"
