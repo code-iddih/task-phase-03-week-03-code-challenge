42593ed7ba347f2a270406309a68c1f84422cc48
code-iddih <idinyamwaya@gmail.com>
2024-09-16 21:47:54 +0300

    Creates the database

--- concerts/main.py
+++ concerts/main.py
@@ -1,15 +1,21 @@
 import os
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import (
+    ForeignKey,
     create_engine,
     Column,
     Integer,
-    String
+    String,
+    Table
+)
+
+from sqlalchemy.orm import (
+    relationship
 )
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
-connection_str = 'sqlite:///' + os.join(BASE_DIR , 'concert.db')
+connection_str = 'sqlite:///' + os.path.join(BASE_DIR , 'concert.db')
 
 engine = create_engine(connection_str)
 
@@ -38,6 +44,12 @@ class Band:
     id = Column(Integer() , primary_key = True)
     name = Column(String() , nullable = False)
     hometown = Column(String() , nullable = False)
+    # Defining the relationship
+    # venues = relationship(
+    #     'Venue', 
+    #     secondary = concerts_table,
+    #     back_populates = 'bands'
+    # )
 
     def __rep__(self):
         return f"<The Customer is : {self.name}>"
@@ -47,6 +59,26 @@ class Venue:
     id = Column(Integer() , primary_key = True)
     title = Column(String() , nullable = False)
     city = Column(String() , nullable = False)
+    # Defining the relationship
+    # bands = relationship(
+    #     'Band',
+    #     secondary = concerts_table,
+    #     back_populates = 'venues'
+    # )
 
     def __rep__(self):
         return f"<The Product is : {self.name}>"
+    
+# The association table
+
+# concerts_table = Table(
+#     'concerts',
+#     Base.metadata,
+#     Column('band_id' , ForeignKey('bands.id'))
+#     Column('venue_id' , ForeignKey('venues.id'))
+# )
+
+# Base class
+Base.metadata.create_all(engine)
+
+
